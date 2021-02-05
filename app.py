420993780681d29362dc7653f0d3e32bb34fef8b
Cade Curry <cade@Chriss-MacBook.local>
2021-02-04 16:15:35 -0800

    first time working in months

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
-from flask import Flask
+from flask import Flask, send_file, render_template
+
+DEVELOPMENT_ENV  = True
+
 app = Flask(__name__)
+
+app.static_folder = 'static'
+
+app_data = {
+    "name":         "Cade's Starter Template for a Flask Web App",
+    "description":  "A basic Flask app using bootstrap for layout",
+    "author":       "Cade Curry",
+    "html_title":   "Cade's Starter Template for a Flask Web App",
+    "project_name": "Outside",
+    "keywords":     "flask, webapp, template, basic"
+}
+
 
 @app.route('/')
 def index():
     return render_template('index.html', app_data=app_data)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', app_data=app_data)
+
+
+@app.route('/mission')
+def service():
+    return render_template('mission.html', app_data=app_data)
+
+
+@app.route('/design')
+def design():
+    return render_template('design.html', app_data=app_data)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=DEVELOPMENT_ENV)
